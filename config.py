3e23805d5411bcfb4ebaf10eaa16b616060f49b3
henriupton99 <72378839+henriupton99@users.noreply.github.com>
2023-07-14 17:57:01 +0200

    add config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+class CONFIG:
+    train_sequences_path = MAIN_DIR  + "/Train/train_sequences.fasta"
+    train_labels_path = MAIN_DIR + "/Train/train_terms.tsv"
+    test_sequences_path = MAIN_DIR + "/Test (Targets)/testsuperset.fasta"
+    ia_path = MAIN_DIR + "/IA.txt"
+    
+    aspects = ["BPO", "CCO", "MFO"]
+    aspects_num_labels = {"BPO" : 713, "CCO" : 151, "MFO" : 136}
+    max_go_terms = 1000
+    
+    n_epochs = 2
+    batch_size = 512
+    lr = 0.001
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
